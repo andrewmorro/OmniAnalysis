368d466c0b29635edea0029907a8fc8af854b8f9
Andrew Wu <andrewmorro@gmail.com>
2017-08-12 19:02:34 +0800

    add rules

--- main.py
+++ main.py
@@ -17,8 +17,33 @@ else:
     stock_list = ts.get_today_all()['code']
     # print(len(stock_list))
 
+stock = '000856'
+date = '2017-04-25'
+day = ts.get_hist_data(code=stock, start=date,end=date)
+pre_close = day['close'].values[0]-day['price_change'].values[0]
 
-print(data)
+
+
+df = ts.get_tick_data(stock, date=date)
+df = df.sort_values(by='time')
+time = datetime.time(9, 25)
+
+# change to use sub string to match time - should achieve speed optimization
+print(datetime.datetime.now())
+for index, row in df.iterrows():
+    temp_time = datetime.datetime.strptime(row['time'], '%H:%M:%S').time()
+    if temp_time >= time:
+        print(row['price'])
+        if time == datetime.time(9, 25):
+            time = datetime.time(9, 30)
+        elif time == datetime.time(11, 30):
+            time = datetime.time(13, 00)
+        else:
+            time = (datetime.datetime.combine(datetime.date.today(),time) + datetime.timedelta(minutes=1)).time()
+
+
+print(datetime.datetime.now())
+
 # rule_name = 'DoubleTopClose'
 #
 # df = tt.analysisByRuleName(rule_name,stock_list,start='2017-04-01')
